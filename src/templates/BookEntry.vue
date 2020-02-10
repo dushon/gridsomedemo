3c80ec443c026adc1c08183ad1629def39beaa6a
Jan Dusek <jan.dusek@digitalcz.cz>
2020-02-10 09:51:23 +0100

    Initial commit

---
+++ src/templates/BookEntry.vue
@@ -0,0 +1,63 @@
+<template>
+  <Layout>
+    <div class="book">
+      <h2>
+        <a :href="$page.bookEntry.url">{{ $page.bookEntry.title }}</a>
+      </h2>
+
+      <div>
+          <div class="img-wrapper">
+            <g-image  :src="$page.bookEntry.cover.large" />
+          </div>
+      </div>
+
+      <div class="book-footer">
+        <div>{{ $page.bookEntry.publish_date }}</div>
+        <div>By <span v-html="$page.bookEntry.authors[0].name" /></div>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query Book ($id: String!){
+  bookEntry (id: $id) {
+    id
+    title
+    authors {
+      name
+    },
+    cover {
+      large
+    }
+    by_statement
+    publish_date
+  }
+}
+</page-query>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Detail'
+  }
+}
+</script>
+
+<style>
+.book {
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1);
+  padding: 0 15px 20px;
+  margin: 20px 5px;
+}
+.img-wrapper {
+  text-align: center;
+}
+.book-footer {
+  margin-top: 30px;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
